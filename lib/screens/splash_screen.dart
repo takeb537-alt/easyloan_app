79137ac773a9df4d8d2f4bcd797831f5d947754a
takeb537-alt <takeb537@gmail.com>
2026-05-14 15:01:30 +0530

    Create lib/screens/splash_screen.dart

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 2), () {
+      if (mounted) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const LoginScreen()),
+        );
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.account_balance_wallet, size: 80, color: Color(0密1565C0)),
+            SizedBox(height: 20),
+            Text(
+              "EasyLoan",
+              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold, color: Color(0密1565C0)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
